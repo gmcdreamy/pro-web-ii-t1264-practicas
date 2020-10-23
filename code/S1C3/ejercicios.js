f9b429841d4712086a747acc123ef9570596638c
gmcdreamy <anibal.quill@gmail.com>
2020-10-23 00:14:50 -0600

    comenzar practica semana2

--- code/S1C3/ejercicios.js
+++ code/S1C3/ejercicios.js
@@ -5,9 +5,33 @@
 // además concatenear la frase "... Leer más"
 // Ej.
 
-function concatenar(n){
-let texto = "El comando checkout se puede usar para crear ramas o cambiar entre ellas. Por ejemplo, el siguiente comando crea una nueva y se cambia a ella"
-return texto.substring(0,n) + "... leer mas" ;
+function concatenar(texto, n) {
+  return texto.substring(0, n) + "... leer mas";
 }
 
-console.log(concatenar(25));
+console.log(concatenar("estamos haciendo el ejercicio numero uno",10));
+
+// 2. vocales
+// dado una cadena de texto de n caracteres
+// buscar en cada palabra que compone dicha cadena la última vocal en la
+// presente palabra y reemplazarla por una vocal pasada como argumento
+// Ej.
+/**
+ * const input = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`; 
+ * vocal pasada como argumento f(a)
+   output =  'Loram ipsam dolar sat amat, consectetar adipiscang elat.'
+   ** nótese     ^     ^     ^   ^    ^            ^         ^     ^  
+ */
+let texto = "segundo ejercicio";
+ function vocales(a){
+let lista_vocales = 'aeiouAEIOU';
+
+texto.forEach(element => {
+//     if (lista_vocales.indexOf(element) !== -1)
+//     {
+        console.log(element);
+//       //es vocal
+//     }
+   });
+ }
+ console.log(vocales("T"));
