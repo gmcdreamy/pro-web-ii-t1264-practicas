f009399d13747e7ac7044e8478402b0e2129240d
gmcdreamy <anibal.quill@gmail.com>
2020-10-28 05:01:11 -0600

    instalado nodemon

--- code/Aplicaciones_web1/holaserver.js
+++ code/Aplicaciones_web1/holaserver.js
@@ -1,20 +1,20 @@
-// const http = require('http')
-// const hostname = '127.0.0.1' //localhost
-// const port = 3000
-// const server = http.createServer((req, res)=>{
-//     res.statusCode = 200;
-//     res.setHeader('Content-Type','text/plain');
-//     res.end('hola node.js');
-// });
-
 const http = require('http')
 const hostname = '127.0.0.1' //localhost
 const port = 3000
 const server = http.createServer((req, res)=>{
     res.statusCode = 200;
-    res.setHeader('Content-Type','application/json');
-    res.end('{"mensaje":"hola node.js"}');
+    res.setHeader('Content-Type','text/plain');
+    res.end('hola node.js');
 });
+
+// const http = require('http')
+// const hostname = '127.0.0.1' //localhost
+// const port = 3000
+// const server = http.createServer((req, res)=>{
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type','application/json');
+//     res.end('{"mensaje":"hola node.js"}');
+// });
 
 server.listen(port,hostname, () =>{
     console.log(`aplicacion corriendo en http//:${hostname}:${port}`)
